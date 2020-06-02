0a91d406d9e81fc16776b5db6a43da63a4578177
Fred Lira <fred.lira@gmail.com>
2020-06-02 01:26:38 -0300

    get-news com word-counter

--- get-news.sh
+++ get-news.sh
@@ -1,17 +1,21 @@
-#!/bin/bash
+ #!/bin/bash
 
 echo "get-news.sh"
 echo ""
 
 news_file=news_$(date +"%Y%m%d_%H%M")
 
-echo "news file: $news_file"
+curl -L -H "charset=utf-8" news.google.com/ > $news_file.data
+echo "curl..    OK"
+
+xmllint --html --xpath '//*[@class="DY5T1d"]/text()' $news_file.data > $news_file.titles
+echo "xmllint.. OK"
+
+awk '{for(x=1;$x;++x)print tolower($x) "-"}' $news_file.titles | tr "${PUNCT}" "@" | sed 's/@//g' | sort | uniq -c | sort > $news_file.count
+echo "awk/tr/sed/sort/uniq..     OK"
 echo ""
 
-curl -L -H "charset=utf-8" news.google.com/ > $news_file.data
-echo "curl.. OK"
-
-set echo off
-xmllint --html --xpath '//*[@class="DY5T1d"]/text()' $news_file.data > $news_file.titles
-set echo on
-echo "xmllint.. OK"
+echo "   data: $news_file.data"
+echo " titles: $news_file.titles"
+echo "  count: $news_file.count"
+echo ""
