ca8ea0beaa0c004204943369f7f03d31e1cf7c2a
Fred Lira <fred.lira@gmail.com>
2020-06-03 01:58:28 -0300

    melhorias no .count e .titles

---
+++ clean-count.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sed -i '/ to-/d' $1
+sed -i '/ the-/d' $1
+sed -i '/ with-/d' $1
+sed -i '/ and-/d' $1
+sed -i '/ a-/d' $1
+sed -i '/ in-/d' $1
+sed -i '/ of-/d' $1
+sed -i '/ for-/d' $1
+sed -i '/ new-/d' $1
+sed -i '/ on-/d' $1
+sed -i '/ |-/d' $1
+sed -i '/ after-/d' $1
+sed -i '/ from-/d' $1
+sed -i '/ is-/d' $1
+sed -i '/ over-/d' $1
+sed -i '/ are-/d' $1
+sed -i '/ i-/d' $1
+sed -i '/ that-/d' $1
+sed -i '/ how-/d' $1
+sed -i '/ out-/d' $1
+sed -i '/ as-/d' $1
+
+cat $1
