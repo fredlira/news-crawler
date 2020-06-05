517efe17ac11e4200fc4ad7c3513369542c6f686
Fred Lira <fred.lira@gmail.com>
2020-06-05 12:39:31 -0300

    adicionando fonte new york times

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ echo $0
 ./get-news.sh google news.google.com/ '//*[@class="DY5T1d"]/text()' en --html
 ./get-news.sh onu https://news.un.org/feed/subscribe/pt/news/all/rss.xml '//item/title/text()' pt
 ./get-news.sh fp https://foreignpolicy.com/feed/ '//item/title/text()' en
+./get-news.sh nyt https://www.nytimes.com/svc/collections/v1/publish/http://www.nytimes.com/topic/subject/international-relations/rss.xml '//item/title/text()' en
 
 awk '{for(x=1;$x;++x)print $x}' news_en.titles | tr "${PUNCT}" "@" | sed 's/@//g' | sort | uniq -c | sort > news_en.tokens
 awk '{for(x=1;$x;++x)print $x}' news_pt.titles | tr "${PUNCT}" "@" | sed 's/@//g' | sort | uniq -c | sort > news_pt.tokens
